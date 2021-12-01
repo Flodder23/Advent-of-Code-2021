ca87ff789759cd7ca91cac5855db7bad0cd36f0f
Joe Gibson <joe.gibby@hotmail.co.uk>
2021-12-01 23:45:38 +0000

    Day 1 Part 2

--- day_01/part_2.cpp
+++ day_01/part_2.cpp
@@ -5,8 +5,13 @@
 void part_2() {
 	std::vector<int> input;
 	read_input(input);
-
-	std::string output;
-
-	std::cout << output;
+	int tot_increase = 0;
+	for (int i = 3; i < input.size(); i++) {
+		// if we are checking if a+b+c < b+c+d
+		// we can actually just check if a < d
+		if (input[i-3] < input[i]) {
+			tot_increase ++;
+		}
+	}
+	std::cout << tot_increase;
 }
