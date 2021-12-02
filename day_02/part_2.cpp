72f04ae82695797cdb88989a89abb380cca5d15f
Joe Gibson <joe.gibby@hotmail.co.uk>
2021-12-02 17:14:49 +0000

    Day 2 Part 2

--- day_02/part_2.cpp
+++ day_02/part_2.cpp
@@ -5,11 +5,19 @@
 #include "read_input.cpp"
 
 void part_2() {
+	int x = 0, y = 0, a = 0; // x is across, y is down
 	std::vector<std::string> direction;
 	std::vector<int> length;
 	read_input(direction, length);
 
-	std::string output;
+	for (int i = 0; i < direction.size(); i++) {
+		if      (direction[i] == "d") { a += length[i]; }
+		else if (direction[i] == "u") { a -= length[i]; }
+		else if (direction[i] == "f") {
+			x += length[i];
+			y += a * length[i];
+		}
+	}
 
-	std::cout << output;
+	std::cout << x*y;
 }
