2c1160673919806e57b7c65e2ef3fa49791f0ff3
Joe Gibson <joe.gibby@hotmail.co.uk>
2021-12-04 21:38:55 +0000

    Day 4 Part 2

--- day_04/part_1.cpp
+++ day_04/part_1.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <string>
 
 #include "read_input.cpp"
 
--- day_04/part_2.cpp
+++ day_04/part_2.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <string>
 
 #include "read_input.cpp"
 
 void part_2() {
-	std::vector<int> draw; std::vector<Board> boards;
-	read_input(draw, boards);
-
-	std::string output;
-
-	std::cout << output;
+	std::vector<int> draw; std::vector<Board> board;
+	read_input(draw, board);
+	
+	std::vector<bool> complete(board.size(), false);
+	int c = 0;
+	for (int i = 0; i < draw.size(); i++) {
+		int d = draw[i];
+		for (int j = 0; j < board.size(); j++) {
+			if (!complete[j] && board[j].mark(d)) {
+				if (c == board.size()-1) {
+					std::cout << d*board[j].score();
+					return;
+				}
+				complete[j] = true;
+				c++;
+			}
+		}
+	}
 }
