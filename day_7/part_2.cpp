a9b3114d40f9e8a1887badd92e57ff5bfc7680ed
Joe Gibson <joe.gibby@hotmail.co.uk>
2021-12-08 01:10:59 +0000

    Day 7 Part 2

--- day_7/part_2.cpp
+++ day_7/part_2.cpp
@@ -1,10 +1,29 @@
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
 #include "read_input.cpp"
 
 int part_2(const std::string& filename) {
 	std::vector<int> crabs;
 	read_input(filename, crabs);
 
-	int output;
+	int cost = 0;
+	for (int i = 0; i < crabs.size(); i++) {
+		cost += (crabs[i] * (crabs[i]+1)) / 2;
+	}
+	
+	int min_cost = cost;
+	for (int i = 1; i < crabs.size(); i++) {
+		cost = 0;
+		for (int j = 0; j < crabs.size(); j++) {
+			int n = std::abs(crabs[j] - i);
+			cost += n*(n+1) / 2;
+		}
+		if (cost < min_cost) {
+			min_cost = cost;
+		}
+	}
 
-	return output;
+	return min_cost;
 }
