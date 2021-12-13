885729ba5ed72e4f396d01c1396b90ed11d24516
Joe Gibson <joe.gibby@hotmail.co.uk>
2021-12-13 10:30:11 +0000

    Improved Day 8 Part 2

--- day_08/part_2.cpp
+++ day_08/part_2.cpp
@@ -2,20 +2,32 @@
 #include <string>
 #include <map>
 #include <set>
-#include <iostream>
 
 #include "read_input.cpp"
 
 typedef std::vector<std::set<sc>> vssc;
+typedef std::set<sc> ssc;
 
-int intersect_size(const sc& s1, const sc& s2) {
-	int size = 0;
-	for (char c : s1) {
-		if (s2.find(c)  != s2.end()) {
-			size ++;
+char find_pos_2(const sc& chars_1, const ssc& len_6) {
+for (const sc& chars : len_6) {
+		for (const char& c : chars_1) {
+			if (chars.find(c) == chars.end()) {
+				return c;
+			}
 		}
 	}
-	return size;
+	return ' ';
+}
+
+char find_pos_3(const sc& chars_4, const ssc& len_6) {
+	for (const sc& chars : len_6) {
+		for (const char& c : chars_4) {
+			if (chars.find(c) == chars.end()) {
+				return c;
+			}
+		}
+	}
+	return ' ';
 }
 
 int part_2(const std::string& filename) {
@@ -23,59 +35,68 @@ int part_2(const std::string& filename) {
 	read_input(filename, examples, outputs);
 	// length	| 2 | 3 | 4 |    5    |    6    | 7 |
 	// numbers	| 1 | 7 | 4 | 2, 3, 5 | 0, 6, 9 | 8 |
+	//
+	//  00
+	// 1  2
+	//  33
+	// 4  5
+	//  66
 
 	int sum = 0;
 	for (int i = 0; i < examples.size(); i++) {
 		vsc& example = examples[i];
 		vsc& output = outputs[i];
-		vsc num_chars(10); // map chars to number
-		vssc len_vec_chars(8); // map length to vector of possible char sets
+		vssc len_to_vec_chars(8);
 		
 		for (sc& ex : example) {
-			len_vec_chars[ex.size()].insert(ex);
+			len_to_vec_chars[ex.size()].insert(ex);
 		}
-		num_chars[1] = *len_vec_chars[2].begin();
-		num_chars[4] = *len_vec_chars[4].begin();
-		num_chars[7] = *len_vec_chars[3].begin();
-		num_chars[8] = *len_vec_chars[7].begin();
-		for (sc chars : len_vec_chars[5]) {
-			if (intersect_size(num_chars[1], chars) == 2) {
-				num_chars[3] = chars;
-				len_vec_chars[5].erase(chars);
-				break;
+		sc chars_1 = *len_to_vec_chars[2].begin();
+		sc chars_4 = *len_to_vec_chars[4].begin();
+		char pos_2 = find_pos_2(chars_1, len_to_vec_chars[6]);
+		char pos_5;
+		for (const char& c : chars_1) {
+			if (c != pos_2) {
+				pos_5 = c;
 			}
+			chars_4.erase(c);
 		}
-		for (sc chars : len_vec_chars[6]) {
-			if (intersect_size(num_chars[3], chars) == 5) {
-				num_chars[9] = chars;
-				len_vec_chars[6].erase(chars);
-				break;
-			}
-		}
-		for (sc chars : len_vec_chars[5]) {
-			if (intersect_size(chars, num_chars[9]) == 5) {
-				num_chars[5] = chars;
-				len_vec_chars[5].erase(chars);
-				break;
-			}
-		}
-		num_chars[2] = *len_vec_chars[5].begin();
-		for (sc chars : len_vec_chars[6]) {
-			if (intersect_size(num_chars[5], chars) == 5) {
-				num_chars[6] = chars;
-				len_vec_chars[6].erase(chars);
-				break;
-			}
-		}
-		num_chars[0] = *len_vec_chars[6].begin();
+		char pos_3 = find_pos_3(chars_4, len_to_vec_chars[6]);
 
-		std::map<sc, std::string> chars_num;
-		for (int i = 0; i <= 9; i++) {
-			chars_num[num_chars[i]] = std::to_string(i);
-		}
 		std::string out = "";
-		for (sc o : output) {
-			out += chars_num[o];
+		for (const sc& o : output) {
+			switch (o.size()) {
+				case 2:
+					out += "1";
+					break;
+				case 3:
+					out += "7";
+					break;
+				case 4:
+					out += "4";
+					break;
+				case 5:
+					if (o.find(pos_2) == o.end()) {
+						out += "5";
+					} else if (o.find(pos_5) == o.end()) {
+						out += "2";
+					} else {
+						out += "3";
+					}
+					break;
+				case 6:
+					if (o.find(pos_2) == o.end()) {
+						out += "6";
+					} else if (o.find(pos_3) == o.end()) {
+						out += "0";
+					} else {
+						out += "9";
+					}
+					break;
+				default:
+					out += "8";
+					break;
+			}
 		}
 		sum += std::stoi(out);
 	}
