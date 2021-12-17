28d942a55a381fa618e686dc8ab4ed2f698d783a
Joe Gibson <joe.gibby@hotmail.co.uk>
2021-12-17 23:26:44 +0000

    Day 17 Part 2

--- day_17/part_2.cpp
+++ day_17/part_2.cpp
@@ -1,10 +1,28 @@
+#include <cmath>
+
 #include "read_input.cpp"
 
 int part_2(const std::string& filename) {
-	int xmin, xmax, ymin, ymax;
-	read_input(filename, xmin, xmax, ymin, ymax);
+	int min_x, max_x, min_y, max_y;
+	read_input(filename, min_x, max_x, min_y, max_y);
 
-	int output;
+	int n = 0;
+	for (int init_vel_x = 0; init_vel_x <= max_x; init_vel_x++) {
+		for (int init_vel_y = min_y; init_vel_y <= -min_y; init_vel_y++) {
+			int pos_x = 0, pos_y = 0, vel_x = init_vel_x, vel_y = init_vel_y;
+			while (pos_x <= max_x && pos_y >= min_y) {
+				pos_x += vel_x;
+				pos_y += vel_y;
 
-	return output;
+				if (min_x <= pos_x && pos_x <= max_x && min_y <= pos_y && pos_y <= max_y) {
+					n ++;
+					break;
+				}
+
+				if (vel_x > 0) { vel_x -= 1; }
+				vel_y -= 1;
+			}
+		}
+	}
+	return n;
 }
